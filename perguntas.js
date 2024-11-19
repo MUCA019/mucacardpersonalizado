884cd559171ea34e05bf9dff634be20f35ae72dc
MUCA019 <00001108865720sp@al.educacao.sp.gov.br>
2024-11-19 11:05:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'neymar',
+    'Quantas copas Neymar jogou?',
+    'neymar jogou 3 copa do mundo'
+)
+
+criaCartao(
+    'neymar',
+    'Quantos gols o Neymar fez em toda sua carreira?',
+    'Neymar contabiliza 439 gols'
+)
+
+criaCartao(
+    'Neymar',
+    'O Neymar se lesionou quantas vezes?',
+    'neymar ja sofreu 30 lesoes'
+)
+
+criaCartao(
+    'Neymar',
+    'Quantas Champions o Neymar tem?',
+    'Neymar contem 5 Champions'
+)
